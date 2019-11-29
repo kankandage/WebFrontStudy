392e7bad050b7dd46c95ee07b7b41fef845d5d48
kankandage <751155732@qq.com>
2019-11-29 22:14:37 +0800

    前端学习

---
+++ vue/vue入门/vuesenior.js
@@ -0,0 +1,39 @@
+
+var [a,b,c,d] = [1,2,3,4,5]
+//对象的解构赋值
+    // var miaov ={
+    //     fn:function(){alert(1)},
+    //     add:function(){alert(2)},
+    //     isFunction:function(){alert(3)},
+    // }
+
+    // let {add,isFunction} = miaov;
+    // console.log(add);
+
+//箭头函数
+//语法： ()=>{}
+
+var f = ()=>{
+    console.log(123);
+}
+
+f()
+
+//返回值为2
+    // var add =()=>2;
+//返回对象需要加()
+    //    var add=()=>({});
+
+//闭包
+var util = function(){
+    function add(){}
+    function isArry(){}
+    miaov
+    return{
+        //对象的简单表示法
+        add,
+        isArry,
+        miaov
+    }
+}()
+
