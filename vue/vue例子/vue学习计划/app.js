35b50e5466299971e6087e57a37d814164f18e21
kankandage <751155732@qq.com>
2020-02-19 17:54:03 +0800

    VUE实战开发1

--- vue/vue例子/vue学习计划/app.js
+++ vue/vue例子/vue学习计划/app.js
@@ -1,23 +1,49 @@
-var list = [
-	{
-		title:"未完成任务",
-		isChecked:false //状态为false，为不选中  任务未完成
+//存取localStorage中的数据
+var store ={
+	save(key,value){
+		localStorage.setItem(key,JSON.stringify(value));
 	},
-	{
-		title:"已完成任务",
-		isChecked:true   //状态为true，为选中    任务完成
+	fetch(key){
+		return JSON.parse (localStorage.getItem(key)) || [];
 	}
-];
+}
 
 
-new Vue({
+//
+// var list = [
+// 	{
+// 		title:"未完成任务",
+// 		isChecked:false //状态为false，为不选中  任务未完成
+// 	},
+// 	{
+// 		title:"已完成任务",
+// 		isChecked:true   //状态为true，为选中    任务完成
+// 	}
+// ];
+
+//取出所有的值
+var list = store.fetch('class');
+
+var vm = new Vue({
 	el:'.main',
 	data:{
 		list:list,
 		todo:"",
 		edtorTodos:'',//记录正在编辑的任务
 		beforeTitle:'',//记录正编辑数据的title
+		visibility:'all', //同属性变化，对数据进行筛选
 		
+	},
+	watch:{
+		// list:function(){ //监控list 属性，当这个属性对应的发生变化行就会执行函数
+		// 	store.save('class',this.list);
+		// }
+		list:{
+			handler:function(){
+				store.save('class',this.list);
+			},
+			deep:true,
+		}
 	},
 	methods:{
 		//逻辑加事件
@@ -73,23 +99,60 @@ new Vue({
 		},
 	},
 	//计算属性
-	computer:{
+	computed:{
 		noCheckedLength:function(){
 			return this.list.filter(function(item){
 				return !item.isChecked;
 			}).length
 		},
+
+
+		//
+		filterList:function(){
+			//过滤的时候三种情况 all unfinished finished 
+			var filter = {
+				all:function(list){
+					return list;
+				},
+				finished:function(list){
+					return list.filter(function(item){
+						return item.isChecked;
+					})
+				},
+				unfinished:function(){
+					return list.filter(function(item){
+						return !item.isChecked;
+					})
+				}
+			}
+			//找到过滤数据，就返回过滤后的数据 没有就返回所有的数据
+			return filter[this.visibility](list)?ilter[this.visibility](list):list;
+		},
 	},
 	//自定义函数
 	directive:{
-		"foucs":{
-			update(el,binding){
-				//el 绑定的元素
-				//binding 
-				if(binding.value){
-					el.focus();//？？？？
-				};
-			},
-		},
+		// "foucs":{
+		// 	update(el,binding){
+		// 		//el 绑定的元素
+		// 		//binding 
+		// 		if(binding.value){
+		// 			el.focus();//？？？？
+		// 		};
+		// 	},
+		// },
 	},
 });
+
+
+
+function watchHashchange(){
+	var hash = window.location.hash.slice(1);
+	vm.visibility = hash;
+	console.log(hash);
+}
+
+
+watchHashchange();
+
+window.addEventListener('hashchange',watchHashchange)
+ 
